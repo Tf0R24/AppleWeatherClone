84ade4a4f6074373dee0b059022684cf39ce83ee
Tf0R24 <tomgreenan05@gmail.com>
2023-09-21 13:42:18 +0100

    Added Geolocation, added weather API
    Added geolocation setup with output as lat and long in console. Added api setup with output as json in console.

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 window.addEventListener('load', function () {
-    fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Leeds?unitGroup=uk&include=days&key=7NXFV8AJEAKU3BYMBQESNEHU8&contentType=json").then((response) => {
-    console.log(response);
-    response.json().then((data) => {
-        console.log(data);
-    });
-});
+
+    geoSetup();
+
+    //API Function
+    function runApi(weather) {
+        console.log(weather);
+        console.log(JSON.parse(JSON.stringify(weather.address)));
+    }
+
+    // GEOLOCATION SETUP
+
+    async function geoSetup(){
+        const successCallback = (position) => {
+            console.log(position.coords.latitude);
+            console.log(position.coords.longitude);
+            apiSetup();
+        };
+        const errorCallback = (error) => {
+            console.log(error);
+        };
+        navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+    }
+
+    // API SETUP
+    async function apiSetup(){
+        console.log("Ive been waiting!");
+        const response = await fetch(
+            "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Leeds?unitGroup=uk&include=days&key=7NXFV8AJEAKU3BYMBQESNEHU8&contentType=json",
+            {
+                method: 'GET',
+                headers: {}
+            }
+        );
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        runApi(data);
+    }
 })
